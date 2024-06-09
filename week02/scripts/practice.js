93d9b21d85b1933c0588b29dffe261e1ffeb8811
Masashi123456 <ota22003@byui.edu>
2024-06-08 22:33:42 -0600

    prove2

--- week02/scripts/practice.js
+++ week02/scripts/practice.js
@@ -4,30 +4,35 @@ const list = document.querySelector('#list');
 
 button.addEventListener('click', (e)=>{
     //prevent the default action of the button
-    e.preventDefault;
+    e.preventDefault();
 
     if (input.value){
+        //Declare elements
         const li = document.createElement('li');
-
         const deleteButton = document.createElement('button');
 
+        //declare values inside of the element
         li.textContent=input.value;
-
         deleteButton.textContent='❌';
         
+        
         li.appendChild(deleteButton);
-
         list.appendChild(li);
 
         deleteButton.addEventListener('click', ()=>{
            
             list.removeChild(li);
+            input.focus();
         })
+
+        input.focus();
+        input.value = '';
 
 
     }else{
         input.focus();
     }
+
 });
 
 
