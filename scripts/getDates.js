c04e7580ac549383a697e7a60fbdd99ec34c6a48
Masashi123456 <ota22003@byui.edu>
2025-01-10 20:25:39 -0700

    scripture

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,6 +1,6 @@
 const currentYearElement = document.querySelector('#year');
-const currentYear = new Date().getFullyYear();
-currentYearElement.innerHTML = `&copy:${currentYear}`;
+const currentYear = new Date().getFullYear();
+currentYearElement.innerHTML = `&copy;${currentYear}`;
 
 const lastModifiedElement = document.querySelector("#lastModified");
 const lastModified = document.lastModified;
