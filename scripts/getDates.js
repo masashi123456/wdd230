df90fecd8c5a35443dff5ba0ce02346527f1845b
Masashi123456 <ota22003@byui.edu>
2025-01-23 23:58:48 -0700

    humber

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -6,3 +6,14 @@ const lastModifiedElement = document.querySelector("#lastModified");
 const lastModified = document.lastModified;
 lastModifiedElement.innerHTML = `Last Modified: <em>${lastModified}</em>`;
 
+document.addEventListener("DOMContentLoaded",()=>{
+    const menu = document.getElementById("menu");
+    const nav = document.querySelector(".navigation1");
+
+    menu.addEventListener("click",(e)=>{
+        e.preventDefault();
+        menu.classList.toggle("show");
+        nav.classList.toggle("show-nav");
+    });
+});
+
