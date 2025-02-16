2295918dff924a181f7687f60ec4f5795b6438fd
Masashi123456 <ota22003@byui.edu>
2025-02-15 18:14:45 -0700

    footcha

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -9,7 +9,7 @@ let Lday = currentDay.toLocaleDateString("en-US",{
 nowDay.textContent = currentDay;
 
 let closeButton = document.querySelector('#close')
-closeButton.addEventListener('click' function(){
+closeButton.addEventListener('click', function(){
     closeButton.toggleClass('.c')
 })
 
@@ -27,3 +27,22 @@ const RandomWeather = () =>{
     document.getElementById('weather').textContent = RandomW;
 
 }
+
+const currentYearElement = document.querySelector('#year');
+const currentYear = new Date().getFullYear();
+currentYearElement.innerHTML = `&copy;${currentYear}`;
+
+const lastModifiedElement = document.querySelector("#lastModified");
+const lastModified = document.lastModified;
+lastModifiedElement.innerHTML = `Last Modified: <em>${lastModified}</em>`;
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const menu = document.getElementById("menu");
+    const nav = document.querySelector(".navigation1");
+
+    menu.addEventListener("click",(e)=>{
+        e.preventDefault();
+        menu.classList.toggle("show");
+        nav.classList.toggle("show-nav");
+    });
+});
