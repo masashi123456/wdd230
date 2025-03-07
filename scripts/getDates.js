184164ad6978c9614d7a351bcc3ceb332add135e
Masashi123456 <ota22003@byui.edu>
2025-03-06 20:01:16 -0700

    color

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -28,3 +28,31 @@ viewCount = Number(viewCount)+1;
 countElement.textContent = viewCount;
 localStorage.setItem("viewPage", viewCount);
 
+document.addEventListener("DOMContentLoaded",()=>{
+    const menu = document.getElementById("menu");
+    const nav = document.querySelector(".navigation1");
+
+    menu.addEventListener("click",(e)=>{
+        e.preventDefault();
+        menu.classList.toggle("show");
+        nav.classList.toggle("show-nav");
+    });
+});
+
+function myFunction(){
+    var x = document.getElementById("navigation");
+    if(x.style.display === "block"){
+        x.style.display="none";
+    }else{
+        x.style.display = "block"
+    }
+}
+
+const nav = document.querySelector('.navigation')
+const respbutton = document.querySelector('#rmenu');
+
+respbutton.addEventListener('click', ()=>{
+    nav.classList.toggle('show');
+    respbutton.classList.toggle('show');
+})
+
