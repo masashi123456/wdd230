dac6aedfb224e8dc2eac33f5e143f6329b1b012c
Masashi123456 <ota22003@byui.edu>
2025-03-14 18:20:43 -0600

    benefitsfixed

---
+++ chamber/scripts/getDates.js
@@ -0,0 +1,36 @@
+const currentYearElement = document.querySelector('#year');
+const currentYear = new Date().getFullYear();
+currentYearElement.innerHTML = `&copy;${currentYear}`;
+
+const lastModifiedElement = document.querySelector("#lastModified");
+const lastModified = document.lastModified;
+lastModifiedElement.innerHTML = `Last Modified: <em>${lastModified}</em>`;
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const menu = document.getElementById("menu");
+    const nav = document.querySelector(".navigation1");
+
+    menu.addEventListener("click",(e)=>{
+        e.preventDefault();
+        menu.classList.toggle("show");
+        nav.classList.toggle("show-nav");
+    });
+});
+
+function myFunction(){
+    var x = document.getElementById("navigation");
+    if(x.style.display === "block"){
+        x.style.display="none";
+    }else{
+        x.style.display = "block"
+    }
+}
+
+const nav = document.querySelector('.navigation')
+const respbutton = document.querySelector('#rmenu');
+
+respbutton.addEventListener('click', ()=>{
+    nav.classList.toggle('show');
+    respbutton.classList.toggle('show');
+})
+
---
+++ chamber/scripts/time.js
@@ -0,0 +1,4 @@
+window.onload=function(){
+    var curr_time = DataTransfer.now();
+    document.querySelector("current").value = curr_time;
+}
